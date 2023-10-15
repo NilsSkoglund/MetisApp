0370453dc91c3fc75aafd952c5969138f7f59a48
puranchen <87152497+puranchen@users.noreply.github.com>
2023-10-15 18:49:19 +0200

    added delete function

--- metis/patients/views.py
+++ metis/patients/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import NewPatientForm
+from .models import Patient
+from django.http import JsonResponse
+import openai
 
 
-# Create your views here.
 def index(request):
-    return render(request, "patients/index.html")
+    if request.method == 'POST':
+        form = NewPatientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('patients:index')
+    patients = Patient.objects.all()
+    form = NewPatientForm()
+    context = {"form": form, "patients": patients}
+    return render(request, "patients/index.html", context)
+
+
+def delete_patient(request, patient_id):
+    try:
+        patient = Patient.objects.get(id=patient_id)
+        patient.delete()
+        return JsonResponse({"status": "success"})
+    except Patient.DoesNotExist:
+        return JsonResponse({"status": "failure", "error": "Patient not found"})
+
